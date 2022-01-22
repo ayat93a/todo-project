c9f8399a1d056007ca09ff4b20cd1371e34343d0
ayat93a <alkayedayat93@gmail.com>
2022-01-23 00:18:45 +0200

    popup

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 alert('Welcome to ToDo list, manage your time with us.');
-var yourName = prompt('Enter your name here : ');
-alert('Nice to meet you, ' + yourName);
+var yourName = prompt("Enter your name here : ");
+var txt = prompt("Enter your gender here : ");
+var num = prompt("Enter your age here : ");
+var confirm = confirm ('are you sure')
 
-
+/*var gtr;
+ function myFunction() {
+    if (txt == female) {
+      grt = "miss"
+    } else if (txt == male) {
+      grt = "mr" ;
+    } else {
+        grt == "please add a defined gender"
+    } 
+};
+alert ('Hi, ' + grt + yourName);*/
+/*
+var age;
+ function myFunction() {
+    if (num = 0 || num <= 0 ) {
+      age = "not correct"; 
+    } else {
+        age = num 
+    } 
+ };
+alert (age) ;*/
