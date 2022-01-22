08c4a837e492b15cb6826bb17b25af3192c78147
ayat93a <alkayedayat93@gmail.com>
2022-01-23 01:10:30 +0200

    dd

--- todo.js
+++ todo.js
@@ -3,26 +3,3 @@ var yourName = prompt("Enter your name here : ");
 var txt = prompt("Enter your gender here : ");
 var num = prompt("Enter your age here : ");
 var confirm = confirm ('are you sure')
-
-/*var gtr;
- function myFunction() {
-    if (txt == female) {
-      grt = "miss"
-    } else if (txt == male) {
-      grt = "mr" ;
-    } else {
-        grt == "please add a defined gender"
-    } 
-};
-alert ('Hi, ' + grt + yourName);
-
-var age;
- function myFunction() {
-    if (num = 0 || num <= 0 ) {
-      age = "not correct"; 
-    } else {
-        age = num 
-    } 
- };
-alert (age) ;
-*/
